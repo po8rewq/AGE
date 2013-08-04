0d2376a3b791f982abe979201458c1cc5b3d7cfd
Adrien Fischer <adrien@revolugame.com>
2013-08-04 23:56:47 +0200

    first commit for the ui part

---
+++ src/age/display/text/TextBaselineEnum.hx
@@ -0,0 +1,15 @@
+/** 
+ * Author: adrien
+ * Date: 04/08/13
+ *
+ * Copyright 2013 - RevoluGame.com
+ */
+
+package age.display.text;
+
+class TextBaselineEnum
+{
+    public function new()
+    {
+    }
+}
---
+++ src/age/display/ui/Button.hx
@@ -0,0 +1,15 @@
+/** 
+ * Author: adrien
+ * Date: 04/08/13
+ *
+ * Copyright 2013 - RevoluGame.com
+ */
+
+package age.display.ui;
+
+class Button
+{
+    public function new()
+    {
+    }
+}
